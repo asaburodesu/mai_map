195cae09ff6571a789d1b444a0ca99cf881bd554
asaburodesu <136838104+asaburodesu@users.noreply.github.com>
2023-10-21 00:41:03 +0900

    Update Category.tsx

--- src/App/Category.tsx
+++ src/App/Category.tsx
@@ -38,7 +38,7 @@ const Content = (props: Props) => {
       <div className="category">
         <div className="container">
           <div className="category-item">
-            <label htmlFor="category-select">カテゴリから選ぶ</label>
+            <label htmlFor="category-select">都道府県から選ぶ</label>
             <Select
               onChange={(e) => {
                 if (e) {
